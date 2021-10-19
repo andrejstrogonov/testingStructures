fabe3ff2c04587002f39761757b71e3b0d461056
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-10-19 16:18:39 +0300

    First repair

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class Main {
 
     public static void main(String[] args) {
-        Queue<String> queue = new LinkedList<String>();
+        MyQueque queue = new MyQueque();
 
     }
 }
