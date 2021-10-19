2a02da8f9345c5baaa2b7e6b7d7b6a971206072d
Андрей Строгонов <strogonovandrej@hotmail.com>
2021-10-19 21:10:14 +0300

    Создание правильной архитектуры

--- src/com/home/MainTest.java
+++ src/com/home/MainTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class MainTest implements MyQueques {
-    private int dim;
-    String[]data=new String[dim];
+class MainTest {
     @BeforeEach
     void setUp() {
     }
@@ -23,32 +21,10 @@ class MainTest implements MyQueques {
 
     }
 
-    @Override
-    public boolean add(String  nullElem) {
-        String[]data1=new String[data.length+1];
-        for (int i = 0; i < data1.length - 1; i++){
-            data1[i < dim ? i : i + 1] = data[i];}
-        data1[dim]= java.lang.String.valueOf(add("nullElem"));
-        return true;
-    }
-
-    @Override
-    public boolean remove(String laselement) {
-        String[]data2=new String[data.length-1];
-        for (int i = 0; i < data2.length - 1; i++){
-            data2[i < dim ? i : i + 1] = data[i];}
-        data2[dim]= java.lang.String.valueOf(remove("laselement"));
-        return true;
+    private void remove(String hello) {
 
     }
-    @Override
-    public void calculatedim(){
-        if (add("nullElem")==true){
-            dim+=1;
-        }
-        if(remove("laselement")==true){
-            dim-=1;
-        }
 
+    private void add(String hello) {
     }
 }
--- src/com/home/MyQueque.java
+++ src/com/home/MyQueque.java
@@ -1,21 +1,33 @@
 package com.home;
 
 public class MyQueque implements MyQueques {
-
+    private int dim;
+    String[]data=new String[dim];
     @Override
     public boolean add(String nullElem) {
-        return Boolean.parseBoolean(("secondelement"));
-
-
+        String[]data1=new String[data.length+1];
+        for (int i = 0; i < data1.length - 1; i++){
+            data1[i < dim ? i : i + 1] = data[i];}
+        data1[dim]= java.lang.String.valueOf(add("nullElem"));
+        return true;
     }
 
     @Override
     public boolean remove(String laselement) {
-
+        String[]data2=new String[data.length-1];
+        for (int i = 0; i < data2.length; i++){
+            data2[i < dim ? i : i + 1] = data[i];}
+        data2[dim]= java.lang.String.valueOf(remove("laselement"));
         return false;
     }
     @Override
     public  void calculatedim(){
+        if (add("nullElem")==true){
+            dim+=1;
+        }
+        if(remove("laselement")==true){
+            dim-=1;
+        }
 
     }
 }
