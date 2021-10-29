af8adcc39520c62e86e00c1c2ba89159d3cb6d0c
Andrej <57407131+andrejstrogonov@users.noreply.github.com>
2021-10-29 13:57:39 +0300

    Delete MyQueques.java

--- src/com/home/MyQueques.java
+++
@@ -1,9 +0,0 @@
-package com.home;
-
-public interface MyQueques {
-    String userElement();
-    String removeUser();
-    boolean offerUser(String elem);
-    String peekUser(String  elem);
-    String pollUser();
-}
